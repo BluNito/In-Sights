51daba0f2bb1ed4044a459ad1f849c9bf005a773
Mukul P <mukul.p.mohan@gmail.com>
2020-09-13 14:18:58 +0530

    Prepared for deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const user = require("./routes/user");
 const stories = require("./routes/stories");
@@ -26,6 +27,14 @@ mongoose
 //Server routes
 app.use("/api/user", user);
 app.use("/api/stories", stories);
+
+//server routes in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve((__dirname, "client", "build", "index.html")));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
